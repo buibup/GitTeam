4caa691b58667bc4e0541b3e72cd911ede62119b
bui bup <bui bup>
2016-12-21 09:00:25 +0700

    add patient model, index.html, edit readme.md

---
+++ Models/Patient.cs
@@ -0,0 +1,8 @@
+class Patient
+{
+ public int Id { get; set; }
+ public string Papmi_no { get; set; }
+ public string Papmi_Name { get; set; }
+ public string Papmi_Name2 { get; set; }
+ public string Papmi_Name3  { get; set; }   
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GitTeam
+
+# MVC Project Team
+
+>21 Dec 2016 <br>
+>First times we create folder structure 
